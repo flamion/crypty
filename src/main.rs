3866515ea132f7b1cb232e8b65042f16a106d436
flamion <flamion@protonmail.com>
2022-07-13 15:54:05 +0200

    Add encoder_v2 encryption/decryption

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,48 @@
+use std::fs::File;
 use std::path::PathBuf;
+
 use clap::Parser;
+use lazy_static::lazy_static;
 use tracing::{debug, error};
 use tracing_core::Level;
 use tracing_subscriber::EnvFilter;
 
+mod v2;
+
+lazy_static! {
+	static ref ARGS: Args = Args::parse();
+}
+
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 pub struct Args {
-    /// Toggles whether debug information should be shown
-    #[clap(short = 'D', long)]
+	/// Toggles whether debug information should be shown.
+	#[clap(short = 'D', long)]
 	#[clap(default_value = "false")]
 	debug: bool,
-    /// Which version of encryption to use
+	/// Which version of encryption to use.
+	#[clap(short, long, long_help = clap_version_long_help())]
+	version: String,
+	/// The file to be encrypted/decrypted.
+	#[clap(short = 'f', long)]
+	source_file: PathBuf,
+	/// The keyfile to be used.
 	#[clap(short, long)]
-    version: String,
-	/// The file to be encrypted/decrypted
-	#[clap(short, long)]
-	file: PathBuf,
-	/// The keyfile to be used
-	#[clap(short, long)]
-	key: PathBuf,
-	/// Whether to decrypt or encrypt
+	key_file: PathBuf,
+	/// Encrypted/Decrypted output file.
+	#[clap(short = 'o', long)]
+	out_file: PathBuf,
+	/// Whether to decrypt or encrypt.
 	#[clap(short, long)]
 	#[clap(default_value = "false")]
 	decrypt: bool,
 }
 
 fn main() {
-	let args = Args::parse();
-
 	let mut env_filter = EnvFilter::from_default_env()
 		.add_directive(Level::INFO.into());
 
-	if args.debug {
+	if ARGS.debug {
 		env_filter = env_filter.add_directive("encoder_v3=debug".parse().unwrap());
 	} else {
 		env_filter = env_filter.add_directive("encoder_v3=info".parse().unwrap());
@@ -44,14 +54,25 @@ fn main() {
 		.with_file(true)
 		.init();
 
-	match args.version.to_lowercase().as_str() {
+	let mut source_file = File::open(&ARGS.source_file).expect("Couldn't open source file.");
+	let mut key_file = File::open(&ARGS.key_file).expect("Couldn't open key file.");
+	let mut out_file = File::open(&ARGS.out_file).expect("Couldn't open out file.");
+
+	if let Err(why) = match ARGS.version.to_lowercase().as_str() {
 		"v2" => {
 			debug!("Choosing version 2");
-
-		},
-		version @ _ => { error!("Invalid version: {version}") }
+			v2::process_file(&mut source_file, &mut key_file, &mut out_file)
+		}
+		version => {
+			error!("Invalid version: {version}");
+			std::process::exit(68);
+		}
+	} {
+		error!("An error occurred while processing: {why}");
 	}
-
-
 }
 
+
+const fn clap_version_long_help() -> &'static str {
+	"Which version of encryption to use\nAvailable versions: 'v2'"
+}
---
+++ src/v2.rs
@@ -0,0 +1,71 @@
+use std::fs::File;
+use std::io;
+use std::io::{Read, Write};
+
+use rand::distributions::{Uniform, Distribution};
+
+use tracing::error;
+
+use crate::ARGS;
+
+const BUFFER_SIZE: usize = 16384;
+
+
+pub fn process_file(source_file: &mut File, key_file: &mut File, out_file: &mut File) -> Result<(), io::Error> {
+	let mut source_buffer = [0; BUFFER_SIZE];
+	let mut key_buffer = [0; BUFFER_SIZE];
+
+	if ARGS.decrypt {
+		loop {
+			let source_bytes_read = source_file.read(&mut source_buffer)?;
+			let key_bytes_read = key_file.read(&mut key_buffer)?;
+
+			if source_bytes_read != key_bytes_read {
+				error!("Amount of bytes read from source file different from amount of bytes read from key file!");
+				// panic!("source_bytes_read != key_bytes_read");
+				std::process::exit(69);
+			}
+
+			if source_bytes_read == 0 {
+				break;
+			}
+
+			xor_with_key(&mut source_buffer, &key_buffer, source_bytes_read);
+
+			out_file.write_all(&source_buffer[..source_bytes_read])?;
+		}
+	} else {
+		loop {
+			let source_bytes_read = source_file.read(&mut source_buffer)?;
+
+			if source_bytes_read == 0 {
+				break
+			}
+
+			generate_key(&mut key_buffer, source_bytes_read);
+
+			xor_with_key(&mut source_buffer, &key_buffer, source_bytes_read);
+
+			out_file.write_all(&source_buffer[0..source_bytes_read])?;
+		}
+	}
+
+
+	Ok(())
+}
+
+fn xor_with_key(source: &mut [u8], key: &[u8], amount_source_bytes: usize) {
+	for i in 0..amount_source_bytes {
+		source[i] ^= key[i];
+	}
+}
+
+/// Generates (part of) the key used for encrypting the file.
+fn generate_key(key: &mut [u8], amount: usize) {
+	let mut rng = rand::thread_rng();
+	let uniform_distribution: Uniform<u8> = Uniform::new_inclusive(0, 255);
+	for i in 0..amount {
+		key[i] = uniform_distribution.sample(&mut rng);
+	}
+}
+
